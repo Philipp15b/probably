5580d9996d031ab7a05459fc43df6e48ea522e24
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2021-07-14 13:36:13 +0200

    implemented a while support for a threshold probability expand.

--- probably/analysis/discrete.py
+++ probably/analysis/discrete.py
@@ -32,7 +32,37 @@ def loopfree_gf(instr: Union[Instr, Sequence[Instr]],
         return precf
 
     if isinstance(instr, WhileInstr):
-        raise Exception("While instruction not supported for cf generation")
+        user_choice = int(input("While Instruction has only limited support. Choose an option:\n"
+              "[1]: Enter an Invariant\n"
+              "[2]: Fix a maximum number of iterations\n"
+              "[3]: Analyse until a certain probability mass is captured (might not terminate!)\n"))
+        if user_choice == 1:
+            invariant = sympy.S(input("Enter the invariant: "))
+            # TODO check the invariant here.
+            pass
+        elif user_choice == 2:
+            max_iter = int(input("Specify a maximum iteration limit: "))
+            sat_part, non_sat_part, approx = _safe_filter(precf, instr.cond)
+            for i in range(max_iter):
+                iterated_part = loopfree_gf(instr.body, sat_part)
+                if iterated_sat + iterated_non_sat == iterated_part and not iterated_approx:
+                    # reached a fixpoint
+                    break
+                iterated_sat, iterated_non_sat, iterated_approx = _safe_filter(iterated_part, instr.cond)
+                non_sat_part += iterated_non_sat
+                sat_part = iterated_sat
+            return non_sat_part
+        elif user_choice == 3:
+            captured_probability_threshold = sympy.S(input("Enter the probability threshold: "))
+            sat_part, non_sat_part, approx = _safe_filter(precf, instr.cond)
+            while non_sat_part.coefficient_sum() < captured_probability_threshold:
+                iterated_part = loopfree_gf(instr.body, sat_part)
+                iterated_sat, iterated_non_sat, iterated_approx = _safe_filter(iterated_part, instr.cond)
+                non_sat_part += iterated_non_sat
+                sat_part = iterated_sat
+            return non_sat_part
+        else:
+            raise Exception(f"Input '{user_choice}' cannot be handled properly.")
 
     if isinstance(instr, IfInstr):
         try:
@@ -97,3 +127,22 @@ def loopfree_gf(instr: Union[Instr, Sequence[Instr]],
         raise NotImplementedError("Dont support TickInstr in CF setting")
 
     raise Exception("illegal instruction")
+
+
+def _safe_filter(input_gf: GeneratingFunction, condition: Expr) -> Tuple[GeneratingFunction, GeneratingFunction, bool]:
+    # TODO move into filter function in GF class
+    try:
+        sat_part = input_gf.filter(condition)
+        non_sat_part = input_gf - sat_part
+        return sat_part, non_sat_part, False
+    except NotComputable as err:
+        print(err)
+        probability = input("Continue with approximation. Enter a probability (0, {}):\t"
+                            .format(input_gf.coefficient_sum()))
+        if probability > 0:
+            approx = input_gf.expand_until(probability)
+            approx_sat_part = approx.filter(condition)
+            approx_non_sat_part = approx - approx_sat_part
+            return approx_sat_part, approx_non_sat_part, True
+        else:
+            raise NotComputable(str(err))
