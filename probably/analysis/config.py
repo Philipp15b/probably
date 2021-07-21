847a8ff6478f2e497eee946ff0911ad501df2b4a
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2021-07-21 17:08:16 +0200

    added config statement

--- probably/analysis/config.py
+++ probably/analysis/config.py
@@ -7,7 +7,10 @@ class ForwardAnalysisConfig:
     use_factorized_duniform: bool = attr.ib(default=True)
     # add more options with their default value here
 
-    show_intermediate_steps: bool = attr.ib(default=False)
+    show_intermediate_steps: bool = attr.ib(default=True)
+
+    # IMPORTANT: Currently has no effect and needs to be set manually in Generating Function class
+    verbose_generating_functions: bool = attr.ib(default=False)
 
     # IMPORTANT: show_ attributes just change the string representation, not the actual computation
     show_rational_probabilities: bool = attr.ib(default= False)
