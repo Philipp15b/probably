686946cd8adcafc1d9c760390cbb22a58b583ad7
Legotier <legotier1999@gmail.com>
2022-12-16 14:52:41 +0100

    Test for function parsing

--- tests/functions/test_function_parsing.py
+++ tests/functions/test_function_parsing.py
@@ -1,4 +1,8 @@
 from probably.pgcl import parse_pgcl
+from probably.pgcl.ast.declarations import FunctionDecl, VarDecl
+from probably.pgcl.ast.expressions import FunctionCallExpr, NatLitExpr, VarExpr
+from probably.pgcl.ast.instructions import AsgnInstr
+from probably.pgcl.ast.types import NatType
 
 
 def test_basic_function():
@@ -11,3 +15,11 @@ def test_basic_function():
         nat x;
         x := f(x := 10);
     """)
+
+    decl = prog.declarations[0]
+    assert isinstance(decl, FunctionDecl)
+    fun = decl.body
+    assert fun.declarations == [VarDecl(var='x', typ=NatType(bounds=None))]
+    assert fun.instructions == [AsgnInstr(lhs='x', rhs=NatLitExpr(10))]
+    assert fun.returns == VarExpr(var='x')
+    assert prog.instructions == [AsgnInstr(lhs='x', rhs=FunctionCallExpr(function='f', input_distr={'x': NatLitExpr(10)}))]
